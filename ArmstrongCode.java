ba711ada32b95a134652db5497ab9922e4489131
Arpit Lekhwar <72155207+ArpitLekhwar@users.noreply.github.com>
2021-10-09 22:12:11 +0530

    Added a new file ArmstrongNumber.java
    Coded the main program in ArmstrongNumber.java

---
+++ ArmstrongCode.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = 0, c, d;
+		System.out.print("Enter the number:");
+		int n = sc.nextInt();
+		c = n;
+		sc.close();
+		for(; n > 0;) {
+			d = n %10;
+			r = r + d * d * d;
+			n = n/10;
+		}
+		if(r == c) {
+			System.out.println("Number is Armstrong.");
+		}
+		else {
+			System.out.println("Number is not Armstrong.");
+		}
+	}
+}
